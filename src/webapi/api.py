3547c06d8c9c52f763127875caa07b0f6f9d3be5
Percy Cortez <50078449+percorcru@users.noreply.github.com>
2022-11-21 18:29:51 -0500

    added cors

--- src/webapi/api.py
+++ src/webapi/api.py
@@ -1,11 +1,13 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_cors import CORS
 import sqlite3
 import sys
 from webapi.process_wrapper import ProcessWrapper
 import subprocess
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
